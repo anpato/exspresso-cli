d0cad698a51757dbb8b89f0ed5b65d44517b5d78
Andre Pato <anpato1994@gmail.com>
2019-12-19 13:22:32 -0500

    updated es descriptions

--- project-init/EsVersionCheck.js
+++ project-init/EsVersionCheck.js
@@ -1,19 +1,19 @@
 import inquirer from 'inquirer'
 
 export const EsVersionCheck = (project_name, next) => {
+  const es6Desc = `ES6: Includes Babel to use "import" and "export" statements, comes packaged with a preconfigured .babelrc`
+  es6Desc.fontcolor('White')
   inquirer
     .prompt({
       type: 'list',
       name: 'esVersion',
-      message: `
-    Which Javascript standard would you like?
-    ES6: Includes Babel to use "import" and "export" statements, comes packaged with a preconfigured .babelrc
-    ES5: Using "module.exports" and non arrow functions
+      message: `Which Javascript standard would you like?
+
+      ES6: Use "Import" and "Export", included .babelrc
+      ES5: Using "module.exports" and non arrow functions
+
     `,
-      choices: [
-        'ES6',
-        'ES5' // TO BE ADDED IN LATER RELEASE
-      ]
+      choices: ['ES6', 'ES5']
     })
     .then(({ esVersion }) => next(project_name, esVersion))
 }
