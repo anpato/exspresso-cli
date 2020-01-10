0274d8cf6139ef11ab5cb86f16a89c8e806435cf
Andre Pato <anpato1994@gmail.com>
2020-01-09 23:58:02 -0500

    unloading stash

--- dist/templates/Es5Templates/Es5Connection.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.Es5Connection = void 0;
-var Es5Connection = "module.exports = function() {\n  const dotenv = require('dotenv')\n  dotenv.config()\n  if (process.env.NODE_ENV === 'production') {\n    return {\n      name: 'Production',\n      connect: process.env.DATABASE_URI\n    }\n  } else {\n    return {\n      name: 'Development',\n      connect: process.env.DEVELOP_URI\n    }\n  }\n}";
-exports.Es5Connection = Es5Connection;
--- dist/templates/Es5Templates/Es5Schema.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.Es5Schema = void 0;
-var Es5Schema = "const CreateModel = require('mongoose').model\nconst Sample = require('./models/Sample')\n\nconst SampleModel = CreateModel('samples', Sample)\n\nmodule.exports = {\n  SampleModel\n}\n";
-exports.Es5Schema = Es5Schema;
